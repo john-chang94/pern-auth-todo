6f27264e0d120198eb030cbd6299133da3a48c58
john-chang94 <j.chang94@yahoo.com>
2020-06-23 23:12:16 -0700

    changed db.sql again

--- db.sql
+++ db.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE authtodolist;
+-- CREATE DATABASE authtodolist;
 
 -- uuid generates a long unique id
 -- must install extension in terminal inside db to run function
@@ -17,8 +17,8 @@ CREATE TABLE todos (
   FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
 
-INSERT INTO users (user_name, user_email, user_password)
-VALUES ('Sam', 'sam@email.com', '1234');
+-- INSERT INTO users (user_name, user_email, user_password)
+-- VALUES ('Sam', 'sam@email.com', '1234');
 
-INSERT INTO todos (user_id, description)
-VALUES ('e53bf943-3058-410e-be99-88ad1d68d8fe', 'sam says hello');
+-- INSERT INTO todos (user_id, description)
+-- VALUES ('e53bf943-3058-410e-be99-88ad1d68d8fe', 'sam says hello');
